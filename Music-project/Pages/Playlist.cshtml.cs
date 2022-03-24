0f02b5160213a35c5d02ef61fa8fec6a1dd44ab3
Your Name <you@example.com>
2022-03-24 11:20:50 +0100

    First revision

---
+++ Music-project/Pages/Playlist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Music_project.Pages
+{
+    public class PlaylistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
