9759e20ebdf2ddcd05f3b37fff7f49445b4f121d
woutvangurp <97080758@st.deltion.nl>
2022-03-28 11:08:38 +0200

    sdfgbhnjmk,l

---
+++ Music-project/NewFolder/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace Music_project.NewFolder
+{
+    public class songMethods
+    {
+        public DataSet GetAllSongs(string file)
+        {
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dtSongs = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColmn dcYear = new DataColmn("year");
+            DataColumn dcGenre =
+        }
+    }
+}
