ba3319e42ffcd65cc8a900e7695842cc08f9fc30
woutvangurp <97080758@st.deltion.nl>
2022-03-31 12:10:11 +0200

    frehsdxdtfyjt

---
+++ Music-project/code/songMethods.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+
+namespace Music_project.NewFolder
+{
+    public class songMethods
+    {
+        public DataSet GetAllSongs(string file)
+        {
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dtSongs = new DataTable("song");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcYear = new DataColumn("year");
+            DataColumn dcGenre = new DataColumn("genre");
+            DataColumn dcTime = new DataColumn("time");
+            
+            dtSongs.Columns.Add(dcId);
+            dtSongs.Columns.Add(dcArtist);
+            dtSongs.Columns.Add(dcTitle);
+            dtSongs.Columns.Add(dcYear);
+            dtSongs.Columns.Add(dcGenre);
+            dtSongs.Columns.Add(dcTime);
+
+            ds.Tables.Add(dtSongs);
+            try
+            {
+                ds.ReadXml(Environment.CurrentDirectory + playlist.xml);
+            }
+            catch
+            { }
+            return ds;
+        }   
+    }
+}
