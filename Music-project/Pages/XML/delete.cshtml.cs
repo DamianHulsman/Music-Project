6fe9f14826d679628f77da5237f4e271fbff5ece
Jonahdewit <97089292@st.deltion.nl>
2022-04-04 11:32:02 +0200

    Delete jonah

---
+++ Music-project/Pages/XML/delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Music_project.Pages.XML
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
