a6caae1ac6c952a18a70d49d70199c85507bd429
woutvangurp <97080758@st.deltion.nl>
2022-04-08 11:27:01 +0200

    deletepage

---
+++ Music-project/Pages/DeleteSong.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Music_project.Pages
+{
+    public class deleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
