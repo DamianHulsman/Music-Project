5a0c20a6a74e5e339f96f6f3673ac5817aa41afe
Wout Gurp <97087058@st.deltion.nl>
2022-05-12 10:38:33 +0200

    song.cs edits

--- Music-project/code/songs.cs
+++ Music-project/code/songs.cs
@@ -2,8 +2,27 @@
 {
     public class Song
     {
+        //constructor
+        public  Song()
+        {
+            title = "leeg";
+            artist = "leeg";
+        }
+
         public string id;
         public string artist;
         public string title;
+        public string year;
+
+        public void Afspelen()
+        {
+            //weet ik nog niet maar komt wel
+            haaldatliedjeergensvandaan();
+        }
+
+        private void haaldatliedjeergensvandaan()
+        {
+
+        }
     }
 }
