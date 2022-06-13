ce89de32ca983d7c92b6e16f73851e00842808c6
Your Name <you@example.com>
2022-06-13 10:18:16 +0200

    alles werkt behalve audio input

--- Music-project/code/songMethods.cs
+++ Music-project/code/songMethods.cs
@@ -75,12 +75,6 @@ namespace Music_project.NewFolder
         }
         public void DeleteSong(string id, string file)
         {
-            //DataRow[] drSongs = ds.Tables["song"].Select("id = '" + id + "'");
-            //if (drSongs != null && drSongs.Length > 0)
-            //{
-            //    drSongs[0].Delete();
-            //    //ds.WriteXml(Environment.CurrentDirectory + "/Data/playlist.xml");
-            //}
             songList.Remove(getSong(int.Parse(id)));
             WriteDataToFile();
         }
